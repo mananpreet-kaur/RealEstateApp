719e78ebfab5cb8070ccf7eb19882d66707e129e
mananpreet-kaur <mananpreetkaur2004@gmail.com>
2025-07-23 18:23:25 +0530

    profile page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import {useSelector} from 'react-redux';
 
 export default function Header() {
-  const {currentUser} = useSelector(state=> state.user)
+  const {currentUser} = useSelector(state => state.user)
   return (
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -29,10 +29,10 @@ export default function Header() {
           <Link to='/profile'>
           {currentUser ? (
                 <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt='profile'/>
-          ):  <li className='text-slate-700 hover:underline '>Sign in</li>
+          ): (<li className='text-slate-700 hover:underline '>Sign in</li>) 
         
         }
-         </Link>  
+           </Link> 
          
         </ul>
       </div>
